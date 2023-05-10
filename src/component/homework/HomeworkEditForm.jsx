f7c894a0c9b583a5a8ad94e5ad1f798c782db989
HanwheeKim <hanwhee2@gmail.com>
2023-05-10 11:13:55 +0900

    #2_230510_2 feature/Student/Note 과제부분 미세수정

--- src/component/homework/HomeworkEditForm.jsx
+++ src/component/homework/HomeworkEditForm.jsx
@@ -11,7 +11,7 @@ import ImgUpload from "../imageUpload/ImgUpload";
 import { api } from "../../api/api";
 
 const HomeworkEditForm = () => {
-  const { answerDetailData, updateStatus } = useSelector(
+  const { answerDetailData, updateStatus, status } = useSelector(
     (state) => state.homework
   );
   const { homeworkAnswerId } = useParams();
@@ -103,93 +103,97 @@ const HomeworkEditForm = () => {
   return (
     <div>
       <div className="table items-center h-5/6 w-40 min-h-40 my-20 mx-20 border-4 border-yellow-300 rounded-3xl">
-        <div className="flex flex-col items-center h-5/6 mt-10 ml-3 mr-3">
-          <div className="text-center font-bold text-3xl mb-5">
-            {answerDetailData?.homeworkQuestionTitle}
-          </div>
-          <div>
-            <div className="flex">
-              <h1 className="float-left font-bold">훈련과정명 :</h1>
-              <div>{answerDetailData?.bootcampName}</div>
-            </div>
-            <div className="flex">
-              <h1 className="float-left font-bold">커리큘럼 :</h1>
-              <div>{answerDetailData?.curriculumName}</div>
-            </div>
-            <div className="flex">
-              <h1 className="float-left font-bold">강사님 :</h1>
-              <div>{answerDetailData?.teacherName}</div>
-            </div>
-            <div className="flex">
-              <h1 className="float-left font-bold">스킬셋 :</h1>
-              <div>{answerDetailData?.skillSetName}</div>
-            </div>
-            <div className="flex">
-              <h1 className="float-left font-bold">기간 :</h1>
-              <div className="float-right ml-3 font-black">
-                <div className="float-left">
-                  {answerDetailData?.homeworkQuestionStartDate}
-                </div>
-                <div>~</div>
-                <div className="float-right">
-                  {answerDetailData?.homeworkQuestionEndDate}
-                </div>
-              </div>
-            </div>
-            <div className="flex">
-              <h1 className="divide-solid float-left font-bold">내용 :</h1>
+        {status === "successed" && (
+          <div className="flex flex-col items-center h-5/6 mt-10 ml-3 mr-3">
+            <div className="text-center font-bold text-3xl mb-5">
+              {answerDetailData?.homeworkQuestionTitle}
             </div>
             <div>
-              <div className="whitespace-pre-wrap break-all overflow-auto">
-                {answerDetailData?.homeworkQuestionContent}
+              <div className="flex">
+                <h1 className="float-left font-bold">훈련과정명 :</h1>
+                <div>{answerDetailData?.bootcampName}</div>
               </div>
-            </div>
-            <div className="flex">
-              <h1 className="divide-solid float-left font-bold">나의 답안 :</h1>
-            </div>
-            <form onSubmit={onSubmit}>
-              <div className="whitespace-pre-wrap break-all overflow-auto">
-                <CKEditor
-                  editor={ClassicEditor}
-                  config={{
-                    className: "w-full",
-                  }}
-                  data={answerDetailData.homeworkAnswerContent}
-                  value={input.homeworkAnswerContent}
-                  onReady={(editor) => {}}
-                  onChange={(event, editor) => {
-                    const data = editor.getData();
-                    setInput({
-                      ...input,
-                      homeworkAnswerContent: data,
-                    });
-                  }}
-                ></CKEditor>
-                {file && (
-                  <div className="mt-5">
-                    <div className="flex flex-col justify-center">
-                      <ImgUpload
-                        file={file.file}
-                        loaded={file.loaded}
-                        delFile={delFile}
-                        fileURL={file.fileURL}
-                        fileChange={fileChange}
-                      />
-                    </div>
+              <div className="flex">
+                <h1 className="float-left font-bold">커리큘럼 :</h1>
+                <div>{answerDetailData?.curriculumName}</div>
+              </div>
+              <div className="flex">
+                <h1 className="float-left font-bold">강사님 :</h1>
+                <div>{answerDetailData?.teacherName}</div>
+              </div>
+              <div className="flex">
+                <h1 className="float-left font-bold">스킬셋 :</h1>
+                <div>{answerDetailData?.skillSetName}</div>
+              </div>
+              <div className="flex">
+                <h1 className="float-left font-bold">기간 :</h1>
+                <div className="float-right ml-3 font-black">
+                  <div className="float-left">
+                    {answerDetailData?.homeworkQuestionStartDate}
                   </div>
-                )}
-              </div>
-              <div className="flex justify-center mt-10">
-                <button className="bg-yellow-300 rounded-lg w-44 h-11 mr-20">
-                  수정하기
-                </button>
-                <div className=" bg-yellow-300 rounded-lg w-44 h-11 text-center">
-                  <Link to={"/homeworkhistory"}>뒤로가기</Link>
+                  <div>~</div>
+                  <div className="float-right">
+                    {answerDetailData?.homeworkQuestionEndDate}
+                  </div>
                 </div>
               </div>
-            </form>
+              <div className="flex">
+                <h1 className="divide-solid float-left font-bold">내용 :</h1>
+              </div>
+              <div>
+                <div className="whitespace-pre-wrap break-all overflow-auto">
+                  {answerDetailData?.homeworkQuestionContent}
+                </div>
+              </div>
+              <div className="flex">
+                <h1 className="divide-solid float-left font-bold">
+                  나의 답안 :
+                </h1>
+              </div>
+              <form onSubmit={onSubmit}>
+                <div className="whitespace-pre-wrap break-all overflow-auto">
+                  <CKEditor
+                    editor={ClassicEditor}
+                    config={{
+                      className: "w-full",
+                    }}
+                    data={answerDetailData.homeworkAnswerContent}
+                    value={input.homeworkAnswerContent}
+                    onReady={(editor) => {}}
+                    onChange={(event, editor) => {
+                      const data = editor.getData();
+                      setInput({
+                        ...input,
+                        homeworkAnswerContent: data,
+                      });
+                    }}
+                  ></CKEditor>
+                  {file && (
+                    <div className="mt-5">
+                      <div className="flex flex-col justify-center">
+                        <ImgUpload
+                          file={file.file}
+                          loaded={file.loaded}
+                          delFile={delFile}
+                          fileURL={file.fileURL}
+                          fileChange={fileChange}
+                        />
+                      </div>
+                    </div>
+                  )}
+                </div>
+                <div className="flex justify-center mt-10">
+                  <button className="bg-yellow-300 rounded-lg w-44 h-11 mr-20">
+                    수정하기
+                  </button>
+                  <div className=" bg-yellow-300 rounded-lg w-44 h-11 text-center">
+                    <Link to={"/homeworkhistory"}>뒤로가기</Link>
+                  </div>
+                </div>
+              </form>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
--- src/component/homework/HomeworkHistoryList.jsx
+++ src/component/homework/HomeworkHistoryList.jsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 import { getHomeworkList } from "../../store/homework/HomeworkSlice";
 
 const HomeworkHistoryList = () => {
-  const { data } = useSelector((state) => state.homework);
+  const { data, status } = useSelector((state) => state.homework);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -30,54 +30,56 @@ const HomeworkHistoryList = () => {
   return (
     <div>
       <div className="table items-center w-max h-5/6 min-w-40 min-h-40 my-20 mx-20 border-4 border-yellow-300 rounded-3xl">
-        <div className="flex flex-col items-center h-5/6 mt-10 ml-3 mr-3">
-          <div className="text-center font-bold text-3xl">과제 내역</div>
-          {data.length === 0 ? (
-            <div>등록된 과제가 없습니다.</div>
-          ) : (
-            <table className="my-10">
-              <thead className="font-bold text-center">
-                <tr>
-                  <td>훈련과정명</td>
-                  <td>커리큘럼</td>
-                  <td>스킬셋</td>
-                  <td>제목</td>
-                  <td>시작일</td>
-                  <td>종료일</td>
-                  <td>상태</td>
-                </tr>
-              </thead>
-              <tbody className="text-center border-t border-sky-400">
-                {data?.map((el) => (
+        {status === "successed" && (
+          <div className="flex flex-col items-center h-5/6 mt-10 ml-3 mr-3">
+            <div className="text-center font-bold text-3xl">과제 내역</div>
+            {data.length === 0 ? (
+              <div>등록된 과제가 없습니다.</div>
+            ) : (
+              <table className="my-10">
+                <thead className="font-bold text-center">
                   <tr>
-                    <td
-                      className="p-1 hover:cursor-pointer"
-                      onClick={() => {
-                        if (el.homeworkAnswerStatus === null)
-                          navigate(`/homeworkwrite/${el.homeworkQuestionId}`);
-                        else navigate(`/homeworkread/${el.homeworkAnswerId}`);
-                      }}
-                    >
-                      {el.bootcampTitle}
-                    </td>
-                    <td className="p-1">{el.curriculumName}</td>
-                    <td className="p-1">{el.skillSetName}</td>
-                    <td className="p-1">{el.homeworkQuestionTitle}</td>
-                    <td className="p-1">{el.homeworkQuestionStartDate}</td>
-                    <td className="p-1">{el.homeworkQuestionEndDate}</td>
-                    <td className="p-1">
-                      {getLinkByStatus(
-                        el.homeworkAnswerStatus,
-                        el.homeworkQuestionId,
-                        el.homeworkAnswerId
-                      )}
-                    </td>
+                    <td>훈련과정명</td>
+                    <td>커리큘럼</td>
+                    <td>스킬셋</td>
+                    <td>제목</td>
+                    <td>시작일</td>
+                    <td>종료일</td>
+                    <td>상태</td>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          )}
-        </div>
+                </thead>
+                <tbody className="text-center border-t border-sky-400">
+                  {data?.map((el) => (
+                    <tr>
+                      <td
+                        className="p-1 hover:cursor-pointer"
+                        onClick={() => {
+                          if (el.homeworkAnswerStatus === null)
+                            navigate(`/homeworkwrite/${el.homeworkQuestionId}`);
+                          else navigate(`/homeworkread/${el.homeworkAnswerId}`);
+                        }}
+                      >
+                        {el.bootcampTitle}
+                      </td>
+                      <td className="p-1">{el.curriculumName}</td>
+                      <td className="p-1">{el.skillSetName}</td>
+                      <td className="p-1">{el.homeworkQuestionTitle}</td>
+                      <td className="p-1">{el.homeworkQuestionStartDate}</td>
+                      <td className="p-1">{el.homeworkQuestionEndDate}</td>
+                      <td className="p-1">
+                        {getLinkByStatus(
+                          el.homeworkAnswerStatus,
+                          el.homeworkQuestionId,
+                          el.homeworkAnswerId
+                        )}
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
+          </div>
+        )}
       </div>
     </div>
   );
